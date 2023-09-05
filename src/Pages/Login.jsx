11bb9d60a74caf6f0433a5719335ddeba38ced45
Ayio <ayoolaajao215@gmail.com>
2023-09-05 23:29:49 +0100

    LandingPage, SignUp, Login page request.

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,10 +2,24 @@ import "../CSS files/Login.css";
 
 const Login = () => {
   return (
-    <>
-      {/* {html goes} */}
-      <div>Login</div>
-    </>
+    <div>
+      <div className="login-body">
+        <img src="images/logo.png" alt="" className="login-logo" />
+        <div className="container-login">
+          <h1 className="login">Welcome back!</h1>
+          <form action="" className="form-login">
+            <div className="login-input">
+              <input type="email" id="email" placeholder="Email" autoComplete="email" required />
+              <input type="password" id="password" placeholder="Password" autoComplete="password" required />
+            </div>
+          </form>
+
+          <p className="privacy">Your privacy and security are our top priorities.</p>
+
+          <button className="login">Log In</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -2,10 +2,48 @@ import "../CSS files/SignUp.css";
 
 const SignUp = () => {
   return (
-    <>
-      {/* {html goes} */}
-      <div>SignUp</div>
-    </>
+    <div>
+      <div className="signup-body">
+        <div>
+          <img src="images/logo.png" alt="Anonymous Logo" className="signup-logo" />
+        </div>
+        <div className="signup-welcome">
+          <h1 className="signup-header">
+            Welcome to <strong className="signup-strong">ANONYMOUS</strong>
+          </h1>
+          <p className="connect">
+            Connect with people anonymously and
+            <br />
+            express yourself freely.
+          </p>
+          <div className="signup-container">
+            <form action="" className="signup-form">
+              <div className="signup-input">
+              <input type="email" id="email" placeholder="Email" required autoComplete="email" />
+              <input type="password" id="password" placeholder="Password" required autoComplete="new-password" />
+              <input type="password" id="confirm" placeholder="Confirm password" required autoComplete="new-password" />
+              </div>
+
+              <div className="checkbox-container">
+                <input type="checkbox" id="checkbox" className="checkbox-input" required />
+                <label htmlFor="checkbox" className="custom-checkbox"></label>
+                <label htmlFor="checkbox">
+                  Agree to our <a href="#" className="terms">terms and services</a>.
+                </label>
+              </div>
+              <button className="signup">Sign Up</button>
+            </form>
+            <footer className="signup-footer">
+              <p>Your privacy and security are our top priorities.</p>
+
+              <p className="signup-footer-text">
+                Already have an account? <a href="login.html" className="login-link">Log In</a>
+              </p>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
